1e30981a5f38788b68266263db0b9f103088cb41
asutoshghanto <asutoshghanto23@gmail.com>
2020-10-14 23:32:34 +0530

    added somefeature

--- client.py
+++ client.py
@@ -1,13 +1,29 @@
 import socket
 
+HEADERSIZE=2
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((socket.gethostname(),1234))
-fullmsg=""
-while True:
-    msg=s.recv(8)
-    if len(msg)<=0:
-        break
-    fullmsg+=msg.decode("utf-8")
 
-print(fullmsg)
+while True:
+
+    fullmsg=""
+    newmsg=True
+
+    while True:
+        msg=s.recv(16)
+        if newmsg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen=int(msg[:HEADERSIZE])
+            print("!!")
+            print(msglen)
+            print("!!")
+            newmsg=False
+        fullmsg+=msg.decode("utf-8")
+
+        if len(fullmsg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(fullmsg[HEADERSIZE:]+"13")
+            new_msg=True
+            fullmsg=""
+    print(fullmsg)
     
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 
+HEADERSIZE=2
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((socket.gethostname(),1234))
 s.listen(5)
@@ -7,5 +8,9 @@ s.listen(5)
 while True:
     clientsocket,address=s.accept()
     print(f"connect from {address} has been established")
-    clientsocket.send(bytes("welcome to the server","utf-8")) 
+
+    msg="welcome to the server!"
+    msg=f"{len(msg):<{HEADERSIZE}}"+msg 
+    print(msg)
+    clientsocket.send(bytes(msg,"utf-8")) 
     clientsocket.close()
